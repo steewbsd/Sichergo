bf1d5b6ceba01e2a2af3ac682bd996d5a14601c1
Daniel P. <daniel@steew.eu>
2025-03-16 11:39:36 +0100

    fw: finish PWM breathing pattern

--- fw/sichergo/src/main.rs
+++ fw/sichergo/src/main.rs
@@ -4,8 +4,44 @@
 use cortex_m_rt::entry;
 use panic_halt as _;
 
+#[derive(Clone, Copy)]
+enum PwmBreathDuty {
+    MAX = 0,
+    STATE1 = 1000,
+    STATE2 = 1500,
+    STATE3 = 2000,
+    STATE4 = 2500,
+    STATE5 = 3000,
+    STATE6 = 3500,
+    STATE7 = 4000,
+    STATE8 = 4500,
+    MIN = 4700,
+}
+
 #[entry]
-fn main () -> ! {
+fn main() -> ! {
+    let breath: [(PwmBreathDuty, u32); 19] = [
+        (PwmBreathDuty::MAX, 100),
+        (PwmBreathDuty::STATE1, 10),
+        (PwmBreathDuty::STATE2, 10),
+        (PwmBreathDuty::STATE3, 10),
+        (PwmBreathDuty::STATE4, 10),
+        (PwmBreathDuty::STATE5, 10),
+        (PwmBreathDuty::STATE6, 10),
+        (PwmBreathDuty::STATE7, 10),
+        (PwmBreathDuty::STATE8, 10),
+        (PwmBreathDuty::MIN, 80),
+        (PwmBreathDuty::STATE8, 10),
+        (PwmBreathDuty::STATE7, 10),
+        (PwmBreathDuty::STATE6, 10),
+        (PwmBreathDuty::STATE5, 10),
+        (PwmBreathDuty::STATE4, 10),
+        (PwmBreathDuty::STATE3, 10),
+        (PwmBreathDuty::STATE2, 10),
+        (PwmBreathDuty::STATE1, 10),
+        (PwmBreathDuty::MAX, 100),
+    ];
+
     let per = stm32f1::stm32f103::Peripherals::take().unwrap();
 
     let rcc = per.RCC;
@@ -13,29 +49,34 @@ fn main () -> ! {
     // Enable HSE (External Oscillator)
     rcc.cr.modify(|_, w| w.hseon().set_bit());
     while rcc.cr.read().hserdy().bit_is_clear() {}
-    
+
     // Enable Peripheral clocks (TIM1 and GPIOB)
-    rcc.apb2enr.write(|w| {
-        w.iopben().set_bit().tim1en().set_bit()
-    });
+    rcc.apb2enr
+        .write(|w| w.iopben().set_bit().tim1en().set_bit());
     // Enable Peripheral clock (TIM2)
     rcc.apb1enr.write(|w| w.tim2en().set_bit());
 
     // Clear gate output by default
-    per.GPIOB.crh.write(|w| w.cnf13().push_pull().mode13().output());
+    per.GPIOB
+        .crh
+        .write(|w| w.cnf13().push_pull().mode13().output());
     per.GPIOB.odr.write(|w| w.odr13().set_bit());
 
     // Set PB13 (Gate Pin) as default alternate mode push pull, default speed
-    per.GPIOB.crh.write(|w| w.mode13().output().cnf13().alt_push_pull());
+    per.GPIOB
+        .crh
+        .write(|w| w.mode13().output().cnf13().alt_push_pull());
 
     // Activate Timer output compare with PWM mode 1 (0b110)
     // Activate Channel 1 Output (0b00)
-    per.TIM1.ccmr1_output().write(|w| unsafe {
-        w.oc1m().bits(0b110).cc1s().bits(0b00)
-    });
+    per.TIM1
+        .ccmr1_output()
+        .write(|w| unsafe { w.oc1m().bits(0b110).cc1s().bits(0b00) });
 
     // Activate Main Output Enable (MOE) for CC1N
-    per.TIM1.bdtr.write(|w| w.moe().set_bit().ossr().clear_bit());
+    per.TIM1
+        .bdtr
+        .write(|w| w.moe().set_bit().ossr().clear_bit());
 
     // Set a count of 1.25s (1/8MHz * 1000 * 10000 = 1.25s)
     let psc = 10;
@@ -49,19 +90,21 @@ fn main () -> ! {
     per.TIM1.cr1.modify(|_, w| w.cen().set_bit());
 
     // Activate complementary output (CC1NE) and disable primary output (CC1E)
-    per.TIM1.ccer.write(|w| w.cc1ne().set_bit().cc1e().clear_bit());
+    per.TIM1
+        .ccer
+        .write(|w| w.cc1ne().set_bit().cc1e().clear_bit());
 
     // Set TIM2 as a base counter for loop delay
-    per.TIM2.arr.write(|w| w.arr().bits(1000));
-    per.TIM2.psc.write(|w| w.psc().bits(1000));
-    
+    per.TIM2.arr.write(|w| w.arr().bits(400));
+    per.TIM2.psc.write(|w| w.psc().bits(200));
+
     // Enable UF/OF interrupt flag
     per.TIM2.dier.write(|w| w.uie().set_bit());
-    
+
     per.TIM2.cr1.write(|w| w.cen().set_bit());
 
-    let mut i = 0;
-    let duties = [200, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 4900, 4900, 4500, 4000, 3500, 3000, 2500, 2000, 1500, 1000, 200, 200, 200, 200];
+    let mut breath_cycle = 0;
+    let mut repetition = 0;
 
     /* ! promise loop */
     loop {
@@ -69,9 +112,16 @@ fn main () -> ! {
         while per.TIM2.sr.read().uif().is_clear() {}
         // Clear flag
         per.TIM2.sr.write(|w| w.uif().clear_bit());
-
-        dc = duties[i];
-        i = (i + 1) % duties.len();
+        let current = breath[breath_cycle];
+        
+        // Iterate over breathing pattern
+        if repetition < current.1 {
+            repetition += 1;
+        } else {
+            breath_cycle = (breath_cycle + 1) % breath.len();
+            repetition = 0;
+        }
+        dc = current.0 as u16;
         per.TIM1.ccr1().write(|w| w.ccr().bits(dc));
     }
 }
