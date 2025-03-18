6c09ea98a64b304daa33ad89fb839522371fed36
Daniel P. <daniel@steew.eu>
2025-03-18 22:14:03 +0100

    fw: Add custom keyberon fw

---
+++ fw/Keyberon/src/matrix.rs
@@ -0,0 +1,138 @@
+//! Hardware pin switch matrix handling.
+
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::InputPin;
+
+/// Describes the hardware-level matrix of switches.
+///
+/// Generic parameters are in order: The type of column pins,
+/// the type of row pins, the number of columns and rows.
+/// **NOTE:** In order to be able to put different pin structs
+/// in an array they have to be downgraded (stripped of their
+/// numbers etc.). Most HAL-s have a method of downgrading pins
+/// to a common (erased) struct. (for example see
+/// [stm32f0xx_hal::gpio::PA0::downgrade](https://docs.rs/stm32f0xx-hal/0.17.1/stm32f0xx_hal/gpio/gpioa/struct.PA0.html#method.downgrade))
+pub struct Matrix<C, R, const CS: usize, const RS: usize>
+where
+    C: OutputPin,
+    R: InputPin,
+{
+    cols: [C; CS],
+    rows: [R; RS],
+}
+
+impl<C, R, const CS: usize, const RS: usize> Matrix<C, R, CS, RS>
+where
+    C: OutputPin,
+    R: InputPin,
+{
+    /// Creates a new Matrix.
+    ///
+    /// Assumes columns are outputs,
+    /// and rows are input pins which are set low when not being scanned.
+    pub fn new<E>(cols: [C; CS], rows: [R; RS]) -> Result<Self, E>
+    where
+        C: OutputPin<Error = E>,
+        R: InputPin<Error = E>,
+    {
+        let mut res = Self { cols, rows };
+        res.clear()?;
+        Ok(res)
+    }
+    fn clear<E>(&mut self) -> Result<(), E>
+    where
+        C: OutputPin<Error = E>,
+        R: InputPin<Error = E>,
+    {
+        for r in self.cols.iter_mut() {
+            r.set_low()?;
+        }
+        Ok(())
+    }
+    /// Scans the matrix and checks which keys are pressed.
+    ///
+    /// Every col pin in order is pulled high, and then each row
+    /// pin is tested; if it's high, the key is marked as pressed.
+    /// Scans the pins and checks which keys are pressed (state is "high").
+    ///
+    /// Delay function allows pause to let input pins settle
+    pub fn get_with_delay<F: FnMut(), E>(&mut self, mut delay: F) -> Result<[[bool; RS]; CS], E>
+    where
+        C: OutputPin<Error = E>,
+        R: InputPin<Error = E>,
+    {
+        let mut keys = [[false; RS]; CS];
+
+        for (ci, col) in self.cols.iter_mut().enumerate() {
+            col.set_high()?;
+            delay();
+            for (ri, row) in self.rows.iter_mut().enumerate() {
+                if row.is_high()? {
+                    keys[ci][ri] = true;
+                }
+            }
+            col.set_low()?;
+        }
+        Ok(keys)
+    }
+
+    /// Scans the matrix and checks which keys are pressed.
+    ///
+    /// Every row pin in order is pulled low, and then each column
+    /// pin is tested; if it's low, the key is marked as pressed.
+    /// Scans the pins and checks which keys are pressed (state is "low").
+    pub fn get<E>(&mut self) -> Result<[[bool; RS]; CS], E>
+    where
+        C: OutputPin<Error = E>,
+        R: InputPin<Error = E>,
+    {
+        self.get_with_delay(|| ())
+    }
+}
+
+/// Matrix-representation of switches directly attached to the pins ("diodeless").
+///
+/// Generic parameters are in order: The type of column pins,
+/// the number of columns and rows.
+pub struct DirectPinMatrix<P, const CS: usize, const RS: usize>
+where
+    P: InputPin,
+{
+    pins: [[Option<P>; RS]; CS],
+}
+
+impl<P, const CS: usize, const RS: usize> DirectPinMatrix<P, CS, RS>
+where
+    P: InputPin,
+{
+    /// Creates a new DirectPinMatrix.
+    ///
+    /// Assumes pins are pull-down inputs. Spots in the matrix that are
+    /// not corresponding to any pins use ´None´.
+    pub fn new<E>(pins: [[Option<P>; RS]; CS]) -> Result<Self, E>
+    where
+        P: InputPin<Error = E>,
+    {
+        let res = Self { pins };
+        Ok(res)
+    }
+
+    /// Scans the pins and checks which keys are pressed (state is "low").
+    pub fn get<E>(&mut self) -> Result<[[bool; RS]; CS], E>
+    where
+        P: InputPin<Error = E>,
+    {
+        let mut keys = [[false; RS]; CS];
+
+        for (ci, col) in self.pins.iter_mut().enumerate() {
+            for (ri, row_option) in col.iter_mut().enumerate() {
+                if let Some(row) = row_option {
+                    if row.is_high()? {
+                        keys[ci][ri] = true;
+                    }
+                }
+            }
+        }
+        Ok(keys)
+    }
+}
--- fw/sichergo/src/main.rs
+++ fw/sichergo/src/main.rs
@@ -1,127 +1,201 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_rt::entry;
 use panic_halt as _;
 
 #[derive(Clone, Copy)]
 enum PwmBreathDuty {
     MAX = 0,
     STATE1 = 1000,
-    STATE2 = 1500,
-    STATE3 = 2000,
-    STATE4 = 2500,
-    STATE5 = 3000,
-    STATE6 = 3500,
-    STATE7 = 4000,
-    STATE8 = 4500,
-    MIN = 4700,
+    STATE2 = 1200,
+    STATE3 = 1500,
+    STATE4 = 1700,
+    STATE5 = 2000,
+    STATE6 = 2300,
+    STATE7 = 2600,
+    STATE8 = 3000,
+    STATE9 = 3300,
+    STATE10 = 3600,
+    STATE11 = 4000,
+    STATE12 = 4500,
+    MIN = 5000,
 }
 
-#[entry]
-fn main() -> ! {
-    let breath: [(PwmBreathDuty, u32); 19] = [
-        (PwmBreathDuty::MAX, 100),
-        (PwmBreathDuty::STATE1, 10),
-        (PwmBreathDuty::STATE2, 10),
-        (PwmBreathDuty::STATE3, 10),
-        (PwmBreathDuty::STATE4, 10),
-        (PwmBreathDuty::STATE5, 10),
-        (PwmBreathDuty::STATE6, 10),
-        (PwmBreathDuty::STATE7, 10),
-        (PwmBreathDuty::STATE8, 10),
-        (PwmBreathDuty::MIN, 80),
-        (PwmBreathDuty::STATE8, 10),
-        (PwmBreathDuty::STATE7, 10),
-        (PwmBreathDuty::STATE6, 10),
-        (PwmBreathDuty::STATE5, 10),
-        (PwmBreathDuty::STATE4, 10),
-        (PwmBreathDuty::STATE3, 10),
-        (PwmBreathDuty::STATE2, 10),
-        (PwmBreathDuty::STATE1, 10),
-        (PwmBreathDuty::MAX, 100),
-    ];
+#[rtic::app(device = stm32f1xx_hal::pac, peripherals = true)]
+mod app {
+    use stm32f1xx_hal::{gpio::*};
 
-    let per = stm32f1::stm32f103::Peripherals::take().unwrap();
+    use super::*;
 
-    let rcc = per.RCC;
+    #[shared]
+    struct Shared {
+        duty_cycle: usize,
+        timer1: stm32f1::stm32f103::TIM1,
+        timer2: stm32f1::stm32f103::TIM2,
+    }
 
-    // Enable HSE (External Oscillator)
-    rcc.cr.modify(|_, w| w.hseon().set_bit());
-    while rcc.cr.read().hserdy().bit_is_clear() {}
+    #[local]
+    struct Local {
+        breath_cycle: usize,
+        repetition: usize,
+        breath_pattern: [(PwmBreathDuty, usize); 27],
+    }
 
-    // Enable Peripheral clocks (TIM1 and GPIOB)
-    rcc.apb2enr
-        .write(|w| w.iopben().set_bit().tim1en().set_bit());
-    // Enable Peripheral clock (TIM2)
-    rcc.apb1enr.write(|w| w.tim2en().set_bit());
+    
+    #[init]
+    fn init(c: init::Context) -> (Shared, Local) {
+        // Enable HSE (External Oscillator)
+        c.device.RCC.cr.modify(|_, w| w.hseon().set_bit());
+        while c.device.RCC.cr.read().hserdy().bit_is_clear() {}
 
-    // Clear gate output by default
-    per.GPIOB
-        .crh
-        .write(|w| w.cnf13().push_pull().mode13().output());
-    per.GPIOB.odr.write(|w| w.odr13().set_bit());
+        // Enable Peripheral clocks (TIM1 and GPIOB)
+        c.device
+            .RCC
+            .apb2enr
+            .write(|w| w.iopben().set_bit().tim1en().set_bit());
+        // Enable Peripheral clock (TIM2)
+        c.device.RCC.apb1enr.write(|w| w.tim2en().set_bit());
 
-    // Set PB13 (Gate Pin) as default alternate mode push pull, default speed
-    per.GPIOB
-        .crh
-        .write(|w| w.mode13().output().cnf13().alt_push_pull());
+        init_led_drive(&c.device);
 
-    // Activate Timer output compare with PWM mode 1 (0b110)
-    // Activate Channel 1 Output (0b00)
-    per.TIM1
-        .ccmr1_output()
-        .write(|w| unsafe { w.oc1m().bits(0b110).cc1s().bits(0b00) });
+        let mut gpb = c.device.GPIOB.split();
 
-    // Activate Main Output Enable (MOE) for CC1N
-    per.TIM1
-        .bdtr
-        .write(|w| w.moe().set_bit().ossr().clear_bit());
+        // Change PB13 into AF PUPD for LED driving (TIM1 was manually)
+        // set up in init_led_drive() )
+        gpb.pb13.into_alternate_push_pull(&mut gpb.crh);
 
-    // Set a count of 1.25s (1/8MHz * 1000 * 10000 = 1.25s)
-    let psc = 10;
-    let cnt = 5000;
-    let mut dc = 0;
-    per.TIM1.psc.write(|w| w.psc().bits(psc));
-    per.TIM1.arr.write(|w| w.arr().bits(cnt));
-    per.TIM1.ccr1().write(|w| w.ccr().bits(dc));
+        let mut gpa = c.device.GPIOA.split();
 
-    // Activate the Timer (duh!)
-    per.TIM1.cr1.modify(|_, w| w.cen().set_bit());
+        let _matrix = keyberon::matrix::Matrix::new(
+            [
+                gpa.pa5.into_push_pull_output(&mut gpa.crl).erase(),
+                gpa.pa6.into_push_pull_output(&mut gpa.crl).erase(),
+                gpa.pa7.into_push_pull_output(&mut gpa.crl).erase(),
+                gpa.pa3.into_push_pull_output(&mut gpa.crl).erase(),
+                gpa.pa4.into_push_pull_output(&mut gpa.crl).erase(),
+            ],
+            [
+                gpb.pb10.into_pull_down_input(&mut gpb.crh).erase(),
+                gpb.pb0.into_pull_down_input(&mut gpb.crl).erase(),
+                gpb.pb1.into_pull_down_input(&mut gpb.crl).erase(),
+                gpb.pb2.into_pull_down_input(&mut gpb.crl).erase(),
+            ]
+        );
 
-    // Activate complementary output (CC1NE) and disable primary output (CC1E)
-    per.TIM1
-        .ccer
-        .write(|w| w.cc1ne().set_bit().cc1e().clear_bit());
+        let bp = [
+            (PwmBreathDuty::MAX, 70),
+            (PwmBreathDuty::STATE1, 10),
+            (PwmBreathDuty::STATE2, 10),
+            (PwmBreathDuty::STATE3, 10),
+            (PwmBreathDuty::STATE4, 10),
+            (PwmBreathDuty::STATE5, 10),
+            (PwmBreathDuty::STATE6, 10),
+            (PwmBreathDuty::STATE7, 10),
+            (PwmBreathDuty::STATE8, 10),
+            (PwmBreathDuty::STATE9, 10),
+            (PwmBreathDuty::STATE10, 10),
+            (PwmBreathDuty::STATE11, 10),
+            (PwmBreathDuty::STATE12, 10),
+            (PwmBreathDuty::MIN, 80),
+            (PwmBreathDuty::STATE12, 10),
+            (PwmBreathDuty::STATE11, 10),
+            (PwmBreathDuty::STATE10, 10),
+            (PwmBreathDuty::STATE9, 10),
+            (PwmBreathDuty::STATE8, 10),
+            (PwmBreathDuty::STATE7, 10),
+            (PwmBreathDuty::STATE6, 10),
+            (PwmBreathDuty::STATE5, 10),
+            (PwmBreathDuty::STATE4, 10),
+            (PwmBreathDuty::STATE3, 10),
+            (PwmBreathDuty::STATE2, 10),
+            (PwmBreathDuty::STATE1, 10),
+            (PwmBreathDuty::MAX, 70),
+        ];
 
-    // Set TIM2 as a base counter for loop delay
-    per.TIM2.arr.write(|w| w.arr().bits(400));
-    per.TIM2.psc.write(|w| w.psc().bits(200));
+        (
+            Shared {
+                duty_cycle: 0,
+                timer1: c.device.TIM1,
+                timer2: c.device.TIM2,
+            },
+            Local {
+                breath_cycle: 0,
+                repetition: 0,
+                breath_pattern: bp,
+            },
+        )
+    }
 
-    // Enable UF/OF interrupt flag
-    per.TIM2.dier.write(|w| w.uie().set_bit());
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        loop {}
+    }
 
-    per.TIM2.cr1.write(|w| w.cen().set_bit());
+    #[task(binds = TIM2,
+           local = [breath_cycle, repetition, breath_pattern],
+           shared = [duty_cycle, timer1, timer2])]
+    fn gate_drive(mut c: gate_drive::Context) {
+        let current = c.local.breath_pattern[*c.local.breath_cycle];
 
-    let mut breath_cycle = 0;
-    let mut repetition = 0;
-
-    /* ! promise loop */
-    loop {
-        // Block until Update event (Delay)
-        while per.TIM2.sr.read().uif().is_clear() {}
-        // Clear flag
-        per.TIM2.sr.write(|w| w.uif().clear_bit());
-        let current = breath[breath_cycle];
-        
         // Iterate over breathing pattern
-        if repetition < current.1 {
-            repetition += 1;
+        if *c.local.repetition < current.1 {
+            *c.local.repetition += 1;
         } else {
-            breath_cycle = (breath_cycle + 1) % breath.len();
-            repetition = 0;
+            *c.local.breath_cycle = (*c.local.breath_cycle + 1) % c.local.breath_pattern.len();
+            *c.local.repetition = 0;
         }
-        dc = current.0 as u16;
-        per.TIM1.ccr1().write(|w| w.ccr().bits(dc));
+        c.shared.duty_cycle.lock(|v| *v = current.0 as usize);
+
+        // Lock and read the current dutycycle value
+        let dc = c.shared.duty_cycle.lock(|dc| *dc);
+
+        // Set the new duty cycle
+        c.shared.timer1.lock(|tim| {
+            tim.ccr1().write(|w| w.ccr().bits(dc as u16));
+        });
+
+        // Clean the timer 2 interrupt flag
+        c.shared.timer2.lock(|tim| {
+            tim.sr.write(|w| w.uif().clear_bit());
+        });
+    }
+
+    // Manual PAC initialization of the PWM Complementary channel as a
+    // workaround for the lack of it on the HAL
+    pub fn init_led_drive(per: &stm32f1::stm32f103::Peripherals) {
+        // Activate Timer output compare with PWM mode 1 (0b110)
+        // Activate Channel 1 Output (0b00)
+        per.TIM1
+            .ccmr1_output()
+            .write(|w| unsafe { w.oc1m().bits(0b110).cc1s().bits(0b00) });
+
+        // Activate Main Output Enable (MOE) for CC1N
+        per.TIM1
+            .bdtr
+            .write(|w| w.moe().set_bit().ossr().clear_bit());
+
+        // Set a count of 1.25s (1/8MHz * 1000 * 10000 = 1.25s)
+        let psc = 10;
+        let cnt = 5000;
+        per.TIM1.psc.write(|w| w.psc().bits(psc));
+        per.TIM1.arr.write(|w| w.arr().bits(cnt));
+        per.TIM1.ccr1().write(|w| w.ccr().bits(0));
+
+        // Activate the Timer (duh!)
+        per.TIM1.cr1.modify(|_, w| w.cen().set_bit());
+
+        // Activate complementary output (CC1NE) and disable primary output (CC1E)
+        per.TIM1
+            .ccer
+            .write(|w| w.cc1ne().set_bit().cc1e().clear_bit());
+
+        // Set TIM2 as a base counter for loop delay
+        per.TIM2.arr.write(|w| w.arr().bits(400));
+        per.TIM2.psc.write(|w| w.psc().bits(200));
+
+        // Enable UF/OF interrupt flag
+        per.TIM2.dier.write(|w| w.uie().set_bit());
+
+        per.TIM2.cr1.write(|w| w.cen().set_bit());
     }
 }
