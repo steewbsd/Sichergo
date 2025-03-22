296db9aaf8ff55723dc6a05c348e38c5514f2bab
Daniel P. <daniel@steew.eu>
2025-03-22 22:09:01 +0100

    fw: add keyberon matrix

--- fw/sichergo/src/main.rs
+++ fw/sichergo/src/main.rs
@@ -23,40 +23,100 @@ enum PwmBreathDuty {
 
 #[rtic::app(device = stm32f1xx_hal::pac, peripherals = true)]
 mod app {
-    use stm32f1xx_hal::{gpio::*};
+    use core::{fmt::Write, iter::Enumerate};
+
+    use keyberon::{debounce::Debouncer, matrix::Matrix};
+    use stm32f1::stm32f103::TIM3;
+    use stm32f1xx_hal::{
+        gpio::{ErasedPin, Input, Output, PullDown, PushPull},
+        prelude::*,
+        serial::{Config, *}, timer::{CounterHz, Event, Timer},
+    };
 
     use super::*;
 
     #[shared]
     struct Shared {
         duty_cycle: usize,
-        timer1: stm32f1::stm32f103::TIM1,
-        timer2: stm32f1::stm32f103::TIM2,
+        matrix: Matrix<ErasedPin<Output<PushPull>>, ErasedPin<Input<PullDown>>, 5, 4>,
+        debouncer: Debouncer<[[bool; 5]; 4]>,
     }
 
     #[local]
     struct Local {
         breath_cycle: usize,
+        timer1: stm32f1::stm32f103::TIM1,
+        timer2: stm32f1::stm32f103::TIM2,
+        timer3: CounterHz<TIM3>,
         repetition: usize,
         breath_pattern: [(PwmBreathDuty, usize); 27],
     }
 
-    
     #[init]
     fn init(c: init::Context) -> (Shared, Local) {
         // Enable HSE (External Oscillator)
-        c.device.RCC.cr.modify(|_, w| w.hseon().set_bit());
-        while c.device.RCC.cr.read().hserdy().bit_is_clear() {}
+        // c.device.RCC.cr.modify(|_, w| w.hseon().set_bit());
+        // while c.device.RCC.cr.read().hserdy().bit_is_clear() {}
 
-        // Enable Peripheral clocks (TIM1 and GPIOB)
+        // Enable Peripheral clocks (TIM1, GPIOA and GPIOB)
         c.device
             .RCC
             .apb2enr
-            .write(|w| w.iopben().set_bit().tim1en().set_bit());
+            .write(|w| w.iopben().set_bit().tim1en().set_bit().iopaen().set_bit());
         // Enable Peripheral clock (TIM2)
         c.device.RCC.apb1enr.write(|w| w.tim2en().set_bit());
 
-        init_led_drive(&c.device);
+        // Activate Timer output compare with PWM mode 1 (0b110)
+        // Activate Channel 1 Output (0b00)
+
+        let rcc = c.device.RCC.constrain();
+
+        let mut flash = c.device.FLASH.constrain();
+        let clocks = rcc.cfgr.sysclk(8.MHz()).freeze(&mut flash.acr);
+
+        // Manual PAC initialization of the PWM Complementary channel as a
+        // workaround for the lack of it on the HAL
+        c.device
+            .TIM1
+            .ccmr1_output()
+            .write(|w| unsafe { w.oc1m().bits(0b110).cc1s().bits(0b00) });
+
+        // Activate Main Output Enable (MOE) for CC1N
+        c.device
+            .TIM1
+            .bdtr
+            .write(|w| w.moe().set_bit().ossr().clear_bit());
+
+        // Set a count of 1.25s (1/8MHz * 1000 * 10000 = 1.25s)
+        let psc = 10;
+        let cnt = 5000;
+        c.device.TIM1.psc.write(|w| w.psc().bits(psc));
+        c.device.TIM1.arr.write(|w| w.arr().bits(cnt));
+        c.device.TIM1.ccr1().write(|w| w.ccr().bits(0));
+
+        // Activate the Timer (duh!)
+        c.device.TIM1.cr1.modify(|_, w| w.cen().set_bit());
+
+        // Activate complementary output (CC1NE) and disable primary output (CC1E)
+        c.device
+            .TIM1
+            .ccer
+            .write(|w| w.cc1ne().set_bit().cc1e().clear_bit());
+
+        // Set TIM2 as a base counter for loop delay
+        c.device.TIM2.arr.write(|w| w.arr().bits(400));
+        c.device.TIM2.psc.write(|w| w.psc().bits(200));
+
+        // Enable UF/OF interrupt flag
+        c.device.TIM2.dier.write(|w| w.uie().set_bit());
+
+        c.device.TIM2.cr1.write(|w| w.cen().set_bit());
+        // Finish PWM init
+
+        // Set up the event tick timer
+        let mut tick_timer = c.device.TIM3.counter_hz(&clocks);
+        tick_timer.start(1.kHz()).unwrap();
+        tick_timer.listen(Event::Update);
 
         let mut gpb = c.device.GPIOB.split();
 
@@ -66,7 +126,30 @@ mod app {
 
         let mut gpa = c.device.GPIOA.split();
 
-        let _matrix = keyberon::matrix::Matrix::new(
+        // Initialize I2C for OLED
+        let _scl = gpa.pa13;
+        let _sda = gpa.pa14;
+
+        // Initialize UART pins
+        let tx = gpa.pa9.into_alternate_push_pull(&mut gpa.crh);
+        let rx = gpa.pa10;
+
+        let mut afio = c.device.AFIO.constrain();
+
+        let mut serial = Serial::new(
+            c.device.USART1,
+            (tx, rx),
+            &mut afio.mapr,
+            Config::default()
+                .baudrate(9600.bps())
+                .wordlength_9bits()
+                .parity_none(),
+            &clocks,
+        );
+
+        // serial.tx.write_str("Initialized device\n").unwrap();
+
+        let kmat = keyberon::matrix::Matrix::new(
             [
                 gpa.pa5.into_push_pull_output(&mut gpa.crl).erase(),
                 gpa.pa6.into_push_pull_output(&mut gpa.crl).erase(),
@@ -79,8 +162,10 @@ mod app {
                 gpb.pb0.into_pull_down_input(&mut gpb.crl).erase(),
                 gpb.pb1.into_pull_down_input(&mut gpb.crl).erase(),
                 gpb.pb2.into_pull_down_input(&mut gpb.crl).erase(),
-            ]
+            ],
         );
+
+        let debnc = Debouncer::new([[false; 5]; 4], [[false; 5]; 4], 5);
 
         let bp = [
             (PwmBreathDuty::MAX, 70),
@@ -115,11 +200,14 @@ mod app {
         (
             Shared {
                 duty_cycle: 0,
-                timer1: c.device.TIM1,
-                timer2: c.device.TIM2,
+                matrix: kmat.unwrap(),
+                debouncer: debnc,
             },
             Local {
                 breath_cycle: 0,
+                timer1: c.device.TIM1,
+                timer2: c.device.TIM2,
+                timer3: tick_timer,
                 repetition: 0,
                 breath_pattern: bp,
             },
@@ -131,9 +219,14 @@ mod app {
         loop {}
     }
 
+    #[task(binds = TIM3,
+           shared = [matrix, debouncer])]
+    fn kbtick(mut c: kbtick::Context) {
+    }
+
     #[task(binds = TIM2,
-           local = [breath_cycle, repetition, breath_pattern],
-           shared = [duty_cycle, timer1, timer2])]
+           local = [breath_cycle, repetition, breath_pattern, timer1, timer2],
+           shared = [duty_cycle])]
     fn gate_drive(mut c: gate_drive::Context) {
         let current = c.local.breath_pattern[*c.local.breath_cycle];
 
@@ -150,52 +243,9 @@ mod app {
         let dc = c.shared.duty_cycle.lock(|dc| *dc);
 
         // Set the new duty cycle
-        c.shared.timer1.lock(|tim| {
-            tim.ccr1().write(|w| w.ccr().bits(dc as u16));
-        });
+        c.local.timer1.ccr1().write(|w| w.ccr().bits(dc as u16));
 
         // Clean the timer 2 interrupt flag
-        c.shared.timer2.lock(|tim| {
-            tim.sr.write(|w| w.uif().clear_bit());
-        });
-    }
-
-    // Manual PAC initialization of the PWM Complementary channel as a
-    // workaround for the lack of it on the HAL
-    pub fn init_led_drive(per: &stm32f1::stm32f103::Peripherals) {
-        // Activate Timer output compare with PWM mode 1 (0b110)
-        // Activate Channel 1 Output (0b00)
-        per.TIM1
-            .ccmr1_output()
-            .write(|w| unsafe { w.oc1m().bits(0b110).cc1s().bits(0b00) });
-
-        // Activate Main Output Enable (MOE) for CC1N
-        per.TIM1
-            .bdtr
-            .write(|w| w.moe().set_bit().ossr().clear_bit());
-
-        // Set a count of 1.25s (1/8MHz * 1000 * 10000 = 1.25s)
-        let psc = 10;
-        let cnt = 5000;
-        per.TIM1.psc.write(|w| w.psc().bits(psc));
-        per.TIM1.arr.write(|w| w.arr().bits(cnt));
-        per.TIM1.ccr1().write(|w| w.ccr().bits(0));
-
-        // Activate the Timer (duh!)
-        per.TIM1.cr1.modify(|_, w| w.cen().set_bit());
-
-        // Activate complementary output (CC1NE) and disable primary output (CC1E)
-        per.TIM1
-            .ccer
-            .write(|w| w.cc1ne().set_bit().cc1e().clear_bit());
-
-        // Set TIM2 as a base counter for loop delay
-        per.TIM2.arr.write(|w| w.arr().bits(400));
-        per.TIM2.psc.write(|w| w.psc().bits(200));
-
-        // Enable UF/OF interrupt flag
-        per.TIM2.dier.write(|w| w.uie().set_bit());
-
-        per.TIM2.cr1.write(|w| w.cen().set_bit());
+        c.local.timer2.sr.write(|w| w.uif().clear_bit());
     }
 }
